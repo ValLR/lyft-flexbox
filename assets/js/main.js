2b09a10a08939d1447b592d8b68627cd658d5ae1
valentina lopez <valentina lopez>
2017-06-04 02:30:49 -0400

    estilos y funcion onscroll, header

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+imagen=document.getElementById("logo-container");
+padre=imagen.parentNode;
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 90) {
+        $(".menu").addClass("active");
+    } else {
+        //remover la propiedad del fondo del nav, así se vuelve transparente de nuevo
+       $(".menu").removeClass("active");
+    }
+});
+
+/*function eliminarElemento(){
+	imagen = document.getElementById("top-logo");	
+	padre = imagen.parentNode;
+	padre.removeChild(imagen);
+	}*/
